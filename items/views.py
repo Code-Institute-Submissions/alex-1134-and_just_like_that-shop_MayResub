fff9074d6063abc7c0d3cd0ae257a8c9cbe1042c
alex-1134 <alexa.t.lach@gmail.com>
2022-02-21 19:24:28 +0000

    Set up Search function

--- items/views.py
+++ items/views.py
@@ -1,13 +1,28 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, reverse, redirect, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Item
+
 
 
 def all_items(request):
 
     items = Item.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Tell us what you're looking for!")
+                return redirect(reverse('items'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            items = items.filter(queries)
 
     context = {
         'items': items,
+        'search_term': query,
     }
 
     return render(request, 'items/items.html', context)
