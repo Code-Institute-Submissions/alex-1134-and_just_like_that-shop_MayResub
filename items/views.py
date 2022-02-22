fb447068d718326ca5f2fefd73453ba4962bec64
alex-1134 <alexa.t.lach@gmail.com>
2022-02-22 18:17:07 +0000

    Add sorting and fix verbose name plural in database

--- items/views.py
+++ items/views.py
@@ -10,6 +10,7 @@ def all_items(request):
     query = None
     sort = None
     direction = None
+    categories = None
 
     if request.GET:
         if 'sort' in request.GET:
@@ -18,19 +19,19 @@ def all_items(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 items = items.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
-            items = items.order_by(sortkey)
-            
+            items = items.order_by(sortkey)   
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             items = items.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
-    if request.GET:
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -40,9 +41,13 @@ def all_items(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             items = items.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'items': items,
         'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'items/items.html', context)
