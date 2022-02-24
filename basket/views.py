d7f6b7c12078fa66d91e628475f6944e856c1338
alex-1134 <alexa.t.lach@gmail.com>
2022-02-24 22:21:00 +0000

    Fix the basket

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_basket(request):
@@ -18,16 +18,36 @@ def add_to_basket(request, item_id):
     if item_id in list(basket.keys()):
         basket[item_id] += quantity
     else:
-        basket[item_id] = quantity
+        basket[item_id] = quantity 
 
     request.session['basket'] = basket
     return redirect(redirect_url)
 
+def adjust_basket(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+
+    basket = request.session.get('basket', {})
+
+    if quantity > 0:
+        basket[item_id] = quantity
+    else:
+        basket.pop(item_id)
+
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
+
+
     
 def item_removal(request, item_id):
     """Remove item from basket"""
+    try: 
+        basket = request.session.get('basket', {})
+        basket.pop(item_id)
 
-    view_basket.delete(item_id)
+        request.session['basket'] = basket
+        return HttpResponse(status=200)
 
-    request.session['basket'] = view_basket
-    return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
