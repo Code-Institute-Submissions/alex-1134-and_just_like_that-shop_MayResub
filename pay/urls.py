6f6ea142704d8e0be481c831c5d2b310d02308ee
alex-1134 <alexa.t.lach@gmail.com>
2022-02-27 17:27:20 +0000

    Configure webhooks

--- pay/urls.py
+++ pay/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
+
 
 urlpatterns = [
     path('', views.pay, name='pay'),
-    path('pay_done/<order_number>', views.pay_done, name='pay_done')
+    path('pay_done/<order_number>', views.pay_done, name='pay_done'),
+    path('wh/', webhook, name='webhook'),
 ]
