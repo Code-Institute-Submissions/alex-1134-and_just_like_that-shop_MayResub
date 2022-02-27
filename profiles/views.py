2dfc1e6a1ec0c425376e4f79a05628ecbce42276
alex-1134 <alexa.t.lach@gmail.com>
2022-02-27 22:35:15 +0000

    Update allauth templates

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
+from .models import UserProfile
+
 def profile(request):
+    profile = get_object_or_404(UserProfile, user=requestuser)
 
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
