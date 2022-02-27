643420357f92b3ee10c6c9a2658adb53603a8728
alex-1134 <alexa.t.lach@gmail.com>
2022-02-27 18:50:16 +0000

    Add caching in payment

--- pay/static/pay/js/stripe.js
+++ pay/static/pay/js/stripe.js
@@ -46,9 +46,57 @@ form.addEventListener('submit', function(ev) {
     ev.preventDefault();
     card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
+
+    var saveInfo = Boolean($('#id-save-info').attr('checked'));
+    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+    var postData = {
+        'csrfmiddlewaretoken': csrfToken,
+        'client_secret': clientSecret,
+        'save_info': saveInfo,
+    };
+    var url = '/pay/pay_checkout_data/';
+
+    $.post(url, postData).done(function () {
+        stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    name: $.trim(form.full_name.value),
+                    phone: $.trim(form.phone_number.value),
+                    email: $.trim(form.email.value),
+                    address:{
+                        line1: $.trim(form.street_address1.value),
+                        line2: $.trim(form.street_address2.value),
+                        country: $.trim(form.country.value),
+                        state: $.trim(form.county.value),
+                    }
+                }
+            },
+            shipping: {
+                name: $.trim(form.full_name.value),
+                phone: $.trim(form.phone_number.value),
+                address: {
+                    line1: $.trim(form.street_address1.value),
+                    line2: $.trim(form.street_address2.value),
+                    country: $.trim(form.country.value),
+                    postal_code: $.trim(form.postcode.value),
+                    state: $.trim(form.county.value),
+                }
+            },
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
+            billing_details: {
+                name: $.trim(form.full_name.value),
+                phone: $.trim(form.phone_number.value),
+                email: $.trim(form.email.value),
+                address:{
+                    line1: $.trim(form.street_address1.value),
+                    line2: $.trim(form.street_address2.value),
+                    country: $.trim(form.country.value),
+                      : $.trim(form.county.value),
+                }
+            }
         }
     }).then(function(result) {
         if (result.error) {
--- pay/urls.py
+++ pay/urls.py
@@ -6,5 +6,6 @@ from .webhooks import webhook
 urlpatterns = [
     path('', views.pay, name='pay'),
     path('pay_done/<order_number>', views.pay_done, name='pay_done'),
+    path('cache_pay_data/', views.cache_pay_data, name='cache_pay_data'),
     path('wh/', webhook, name='webhook'),
 ]
