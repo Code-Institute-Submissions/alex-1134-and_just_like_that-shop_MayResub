6c1eee07cdc0d409aae180381a6149dbd49a5ead
alex-1134 <alexa.t.lach@gmail.com>
2022-03-08 11:17:08 +0000

    Upload json file

--- ajlt/settings.py
+++ ajlt/settings.py
@@ -151,7 +151,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    }
 ]
 
 
@@ -181,7 +181,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = 
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
