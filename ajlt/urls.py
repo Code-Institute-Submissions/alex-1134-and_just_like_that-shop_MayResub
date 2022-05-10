67e03358e30e4781b771c890a05ad5547bdac7b9
alex-1134 <alexa.t.lach@gmail.com>
2022-05-10 21:49:22 +0000

    attempting yet to fix confirm email

--- ajlt/urls.py
+++ ajlt/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('basket/', include('basket.urls')),
     path('pay/', include('pay.urls')),
     path('profile/', include('profiles.urls')),
+    url(r'^accounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
